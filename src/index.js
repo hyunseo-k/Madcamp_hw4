739ea2bc4c9acc0bb87c7a422102d7dc5e32438c
Hyunseo Kang <jelly9999@g.skku.edu>
2023-07-25 14:07:16 +0900

    git 시작

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-import ReactDom from "react-dom"
-import App from "./App"
-import 'mdb-react-ui-kit/dist/css/mdb.min.css';
-import "@fortawesome/fontawesome-free/css/all.min.css";
-
-ReactDom.render(<App/> , document.querySelector('#root'))
